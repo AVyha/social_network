826f4579691ac21e3a978d9a4e5adb5f75672d7d
Artem <Artemvyhovanec@gmail.com>
2023-05-04 07:40:27 +0300

    change import

--- app/auth/manager.py
+++ app/auth/manager.py
@@ -4,7 +4,7 @@ from typing import Optional
 from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, UUIDIDMixin
 
-from auth.database import User, get_user_db
+from app.auth.database import User, get_user_db
 
 SECRET = "SECRET"
 
