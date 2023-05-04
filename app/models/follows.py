b9c36f3be91fabf4fc91d3c861da6a53319a9983
Artem <Artemvyhovanec@gmail.com>
2023-05-04 17:20:25 +0300

    add follow model

---
+++ app/models/follows.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Integer, UUID, ForeignKey, Table, Column
+
+from app.database import Base
+
+
+follows = Table(
+    "followers",
+    Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column("user_id", UUID(as_uuid=True), ForeignKey("user.id")),
+    Column("post_id", Integer, ForeignKey("user.id"))
+)
